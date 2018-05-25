8a5dc7747ef77b3292a0b1a859ddfaef689b8736
Gabriel Gray <grayg@rpi.edu>
2018-05-25 14:02:35 -0400

    CameraMove works for moving up a screen, but not to move back down. It keeps flashing back and forth when it gets to the next screen if you implement moving down

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -17,28 +17,27 @@ public class CameraMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //checking x pos first
-        Debug.Log(player.transform.position + " " + player.transform.position.y % 5 + " " + player.transform.position.y % 10 +" " + m.transform.position + " "+ m.transform.position.y);
+        Debug.Log(player.transform.position +  " " + player.transform.position.y / 10 +" " + m.transform.position + " "+ m.transform.position.y);
         Debug.Log(m.transform.position.y < 10);
         Debug.Log(player.transform.position.y > m.transform.position.y);
         Debug.Log("---------");
-        if(m.transform.position.y < 10  && player.transform.position.y % 10 == 0 && player.transform.position.y > m.transform.position.y) // moving up to next screen
+        if (m.transform.position.y < 15 && player.transform.position.y / 10 >= 1 && player.transform.position.y > m.transform.position.y) // moving up to next screen
         {
-            Debug.Log("In if");
             float t = 10; //for lerping movement
-            Vector2 ini_pos = new Vector2(m.transform.position.x, m.transform.position.y);
-            Vector2 final_pos = new Vector2(m.transform.position.x , m.transform.position.y + 10);
-            m.transform.position = Vector2.Lerp(ini_pos, final_pos, t);
+            Vector3 ini_pos = new Vector3(m.transform.position.x, m.transform.position.y, -1.0f);
+            Vector3 final_pos = new Vector3(m.transform.position.x, m.transform.position.y + 15, -1.0f);
+            m.transform.position = Vector3.Lerp(ini_pos, final_pos, t);
 
         }
-        else if (m.transform.position.y > -10  && player.transform.position.y % 10 == 0 && player.transform.position.y < m.transform.position.y) //moving down to next screen
+        else if (m.transform.position.y > -15 && player.transform.position.y / 10 == 1 && player.transform.position.y < m.transform.position.y) //moving down to next screen
         {
 
         }
-        else if (m.transform.position.x < 18  && player.transform.position.x % 18 == 0 && player.transform.position.x > m.transform.position.x)// moving right to next screen
+        else if (m.transform.position.x < 18 && player.transform.position.x % 18 == 0 && player.transform.position.x > m.transform.position.x)// moving right to next screen
         {
 
         }
-        else if (m.transform.position.x > -18  && player.transform.position.x % 18 == 0 && player.transform.position.x < m.transform.position.x) // moving left to next screen
+        else if (m.transform.position.x > -18 && player.transform.position.x % 18 == 0 && player.transform.position.x < m.transform.position.x) // moving left to next screen
         {
 
         }
