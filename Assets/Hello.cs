90cadc4a97a95964ec3e830cc8c2a351587ed4ef
simonlhopkins <simonlhopkins@gmail.com>
2018-05-25 10:50:33 -0400

    added jibberish to hello world

--- Assets/Hello.cs
+++ Assets/Hello.cs
@@ -6,7 +6,7 @@ public class Hello : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        print("Hello world");
+        print("Hello world jkasdlkjsalkdjaslkdjaslkjdaslkjdaso");
 	}
 	
 	// Update is called once per frame
