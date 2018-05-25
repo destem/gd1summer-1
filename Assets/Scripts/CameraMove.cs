89572516f55c6db39ae53010ba9b9ed7cfa363d8
Gabriel Gray <grayg@rpi.edu>
2018-05-25 14:20:23 -0400

    CameraMove works for positive y coordinates

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -17,21 +17,26 @@ public class CameraMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //checking x pos first
-        Debug.Log(player.transform.position +  " " + player.transform.position.y / 10 +" " + m.transform.position + " "+ m.transform.position.y);
+        Debug.Log(player.transform.position +  " " + player.transform.position.y % 10 +" " + m.transform.position + " "+ m.transform.position.y);
         Debug.Log(m.transform.position.y < 10);
         Debug.Log(player.transform.position.y > m.transform.position.y);
         Debug.Log("---------");
-        if (m.transform.position.y < 15 && player.transform.position.y / 10 >= 1 && player.transform.position.y > m.transform.position.y) // moving up to next screen
+        if (m.transform.position.y < 15  && player.transform.position.y > m.transform.position.y && player.transform.position.y % 10 >= 9 || player.transform.position.y % 10 <= -9) // moving up to next screen
         {
             float t = 10; //for lerping movement
             Vector3 ini_pos = new Vector3(m.transform.position.x, m.transform.position.y, -1.0f);
             Vector3 final_pos = new Vector3(m.transform.position.x, m.transform.position.y + 15, -1.0f);
             m.transform.position = Vector3.Lerp(ini_pos, final_pos, t);
+            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y+1, player.transform.position.z);
 
         }
-        else if (m.transform.position.y > -15 && player.transform.position.y / 10 == 1 && player.transform.position.y < m.transform.position.y) //moving down to next screen
+        else if (m.transform.position.y > -15 && player.transform.position.y < m.transform.position.y && player.transform.position.y % 10 >= 9 || player.transform.position.y % 10 <= -9) //moving down to next screen
         {
-
+            float t = 10; //for lerping movement
+            Vector3 ini_pos = new Vector3(m.transform.position.x, m.transform.position.y, -1.0f);
+            Vector3 final_pos = new Vector3(m.transform.position.x, m.transform.position.y - 15, -1.0f);
+            m.transform.position = Vector3.Lerp(ini_pos, final_pos, t);
+            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - 1, player.transform.position.z);
         }
         else if (m.transform.position.x < 18 && player.transform.position.x % 18 == 0 && player.transform.position.x > m.transform.position.x)// moving right to next screen
         {
