f2baed7f38a87a05623437aa43a9c1bc373c15cb
simonlhopkins <simonlhopkins@gmail.com>
2018-05-26 16:54:20 -0400

    functional 7 frame 12 fps animation for idle, and 4 directions

--- Assets/Scripts/WalkScript.cs
+++ Assets/Scripts/WalkScript.cs
@@ -8,9 +8,13 @@ public class WalkScript : MonoBehaviour {
 
     public GameObject player;
     float speed = 5f;
+    public Animator anim;
+    bool goingUp= false;
+    bool goingRight = false;
+    bool keyDown= false;
 	void Start () {
-		
 
+        anim = GetComponent<Animator>();
 
 	}
 	
@@ -23,20 +27,34 @@ public class WalkScript : MonoBehaviour {
 	}
 
     void movement(){
+        
+        anim.SetBool("keyDown", true);
+        anim.SetBool("goingUp", false);
+        anim.SetBool("goingDown", false);
+        anim.SetBool("goingLeft", false);
+        anim.SetBool("goingRight", false);
         if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d")){
             player.transform.Translate(new Vector3(speed*Time.deltaTime, 0, 0));
+            anim.SetBool("goingRight", true);
+
         }
         else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
         {
             player.transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+            anim.SetBool("goingLeft", true);
         }
         else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w"))
         {
             player.transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+            anim.SetBool("goingUp", true);
         }
         else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s"))
         {
             player.transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+            anim.SetBool("goingDown", true);
+        }
+        else{
+            anim.SetBool("keyDown", false);
         }
 
     }
