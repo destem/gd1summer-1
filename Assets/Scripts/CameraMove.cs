efdf169b3f51f12e42b3192122bf2826f4853c86
Gabriel Gray <grayg@rpi.edu>
2018-05-26 12:58:26 -0400

    Fixed CameraMove for the y axis, working on x axis now

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -22,47 +22,49 @@ public class CameraMove : MonoBehaviour {
         Debug.Log(player.transform.position.y > m.transform.position.y);
         Debug.Log(player.transform.position.y % 10 < -1);
         Debug.Log("---------");
-        if (player.transform.position.x >= 0) {
-            if (m.transform.position.y < 15 && player.transform.position.y > m.transform.position.y && player.transform.position.y % 10 >= 9) // moving up
+        if (player.transform.position.y >= 0) {
+            if (m.transform.position.y < 15 && player.transform.position.y > m.transform.position.y && player.transform.position.y % 10 >= 9.9) // moving up
             {
-                float t = 10; //for lerping movement
+                
                 Vector3 ini_pos = new Vector3(m.transform.position.x, m.transform.position.y, -1.0f); // starting position of camera
                 Vector3 final_pos = new Vector3(m.transform.position.x, m.transform.position.y + 15, -1.0f);// ending position of camera
                 m.transform.position = Vector3.Lerp(ini_pos, final_pos, t); // Lerp and move camera
                 player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 1, player.transform.position.z); // move player out of buffer zone
 
             }
-            else if (m.transform.position.y < -15 && player.transform.position.y < m.transform.position.y && player.transform.position.y % 10 <= 1 || player.transform.position.y % 10 <= -9) //moving down to next screen
+            else if ( player.transform.position.y < m.transform.position.y && player.transform.position.y % 10 <= 0.2 ) //moving down to next screen
             {
-                float t = 10; //for lerping movement
+                
                 Vector3 ini_pos = new Vector3(m.transform.position.x, m.transform.position.y, -1.0f); // starting position of camera
                 Vector3 final_pos = new Vector3(m.transform.position.x, m.transform.position.y - 15, -1.0f);// ending position of camera
                 m.transform.position = Vector3.Lerp(ini_pos, final_pos, t);// Lerp and move camera
                 player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - 1, player.transform.position.z);// move player out of buffer zone
             }
         }
-        else if (player.transform.position.x < 0)
+        else if (player.transform.position.y < 0)
         {
-            if (m.transform.position.y < 15 && player.transform.position.y < m.transform.position.y && player.transform.position.y % 10 <= -1) // moving up to next screen
+            Debug.Log(m.transform.position.y < 15 );
+            Debug.Log(player.transform.position.y > m.transform.position.y);
+            Debug.Log(player.transform.position.y % 10 <= -1);
+            if ( player.transform.position.y > m.transform.position.y && player.transform.position.y % 10 >= -0.2) // moving up to next screen
             {
-                Debug.Log("Should go up");
-                float t = 10; //for lerping movement
+                
                 Vector3 ini_pos = new Vector3(m.transform.position.x, m.transform.position.y, -1.0f); // starting position of camera
                 Vector3 final_pos = new Vector3(m.transform.position.x, m.transform.position.y + 15, -1.0f);// ending position of camera
                 m.transform.position = Vector3.Lerp(ini_pos, final_pos, t);// Lerp and move camera
                 player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 1, player.transform.position.z);// move player out of buffer zone
 
             }
-            else if (m.transform.position.y > -15 && player.transform.position.y > m.transform.position.y && player.transform.position.y % 10 >= -9) //moving down to next screen
+            else if (m.transform.position.y > -15 && player.transform.position.y < m.transform.position.y && player.transform.position.y % 10 <= -9.9) //moving down to next screen
             {
-                float t = 10; //for lerping movement
+                
                 Vector3 ini_pos = new Vector3(m.transform.position.x, m.transform.position.y, -1.0f); // starting position of camera
                 Vector3 final_pos = new Vector3(m.transform.position.x, m.transform.position.y - 15, -1.0f);// ending position of camera
                 m.transform.position = Vector3.Lerp(ini_pos, final_pos, t);// Lerp and move camera
                 player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - 1, player.transform.position.z);// move player out of buffer zone
             }
         }
-        else if (player.transform.position.y >= 0) { 
+        else if (player.transform.position.x >= 0) { 
            if (m.transform.position.x < 18 && player.transform.position.x % 18 == 0 && player.transform.position.x > m.transform.position.x)// moving right to next screen
             {
 
@@ -72,7 +74,7 @@ public class CameraMove : MonoBehaviour {
 
             }
         }
-        else if (player.transform.position.y < 0)
+        else if (player.transform.position.x < 0)
         {
 
         }
