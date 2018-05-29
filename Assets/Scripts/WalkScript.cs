ba189fcf2738582723294c5607ef6ec221688079
simonlhopkins <simonlhopkins@gmail.com>
2018-05-29 11:52:31 -0400

    remade all animations

--- Assets/Scripts/WalkScript.cs
+++ Assets/Scripts/WalkScript.cs
@@ -8,9 +8,10 @@ public class WalkScript : MonoBehaviour {
 
     public GameObject player;
     float speed = 5f;
+    Animator anim;
 	void Start () {
-		
 
+        anim = GetComponent<Animator>();
 
 	}
 	
@@ -23,21 +24,35 @@ public class WalkScript : MonoBehaviour {
 	}
 
     void movement(){
+        anim.SetBool("keyDown", false);
+        anim.SetBool("goingUp", false);
+        anim.SetBool("goingDown", false);
+        anim.SetBool("goingLeft", false);
+        anim.SetBool("goingRight", false);
+
+        if(Input.anyKey){
+            anim.SetBool("keyDown", true);
+        }
         if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d")){
+            anim.SetBool("goingRight", true);
             player.transform.Translate(new Vector3(speed*Time.deltaTime, 0, 0));
         }
         else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
         {
+            anim.SetBool("goingLeft", true);
             player.transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
         }
         else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w"))
         {
+            anim.SetBool("goingUp", true);
             player.transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
         }
         else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s"))
         {
+            anim.SetBool("goingDown", true);
             player.transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
         }
+
 
     }
 }
