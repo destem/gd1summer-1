792c373a532a590720020b4974ca51ef0d640bc3
simonlhopkins <simonlhopkins@gmail.com>
2018-06-01 10:30:48 -0400

    applied the right animation to the player prefab

--- Assets/Scripts/WalkScript.cs
+++ Assets/Scripts/WalkScript.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class WalkScript : MonoBehaviour {
 
     // Use this for initialization
+    [SerializeField]
+    private float playerSpeed= 5f;             //Floating point variable to store the player's movement speed.
 
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+    private Vector2 currentVelocity;
     public GameObject player;
-    float speed = 5f;
     Animator anim;
 	void Start () {
-
+        rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
 
 	}
@@ -24,35 +28,39 @@ public class WalkScript : MonoBehaviour {
 	}
 
     void movement(){
+
+
         anim.SetBool("keyDown", false);
         anim.SetBool("goingUp", false);
         anim.SetBool("goingDown", false);
         anim.SetBool("goingLeft", false);
         anim.SetBool("goingRight", false);
-
+        currentVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         if(Input.anyKey){
             anim.SetBool("keyDown", true);
         }
         if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d")){
             anim.SetBool("goingRight", true);
-            player.transform.Translate(new Vector3(speed*Time.deltaTime, 0, 0));
         }
         else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
         {
             anim.SetBool("goingLeft", true);
-            player.transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
         }
         else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w"))
         {
             anim.SetBool("goingUp", true);
-            player.transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
         }
         else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s"))
         {
             anim.SetBool("goingDown", true);
-            player.transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
         }
 
 
     }
+    void FixedUpdate()
+    {
+        rb2d.velocity = currentVelocity * playerSpeed;
+    }
 }
+
+
