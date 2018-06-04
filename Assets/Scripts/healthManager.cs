84f8e329bf532250f8f67f1d33f556eeb8a6f73d
simonlhopkins <simonlhopkins@gmail.com>
2018-06-04 09:00:38 -0400

    added heart break animation

--- Assets/Scripts/healthManager.cs
+++ Assets/Scripts/healthManager.cs
@@ -8,25 +8,39 @@ public class healthManager : MonoBehaviour {
     HarmfulCollision harmfulCollision;
     public GameObject player;
     int currentLives;
+    bool animationPlaying;
 
 	// Use this for initialization
     void Start () {
+        animationPlaying = false;
         print("initialize");
         harmfulCollision = player.GetComponent<HarmfulCollision>();
         currentLives = harmfulCollision.health;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
-        if(harmfulCollision.Damage){
-            
-            print("deactivate");
 
-        }
+        //if(animationPlaying){
+        //    Animator animator = gameObject.transform.GetChild(currentLives).GetComponent<Animator>();
+        //    if (animator.GetCurrentAnimatorStateInfo(0).IsName("heartBreakAnim"))
+        //    {
+        //        print("done");
+        //        animator.SetBool("takeDamage", false);
+        //        gameObject.transform.GetChild(currentLives).gameObject.SetActive(false);
+        //    } 
+        //}
+
 	}
     public void loseLife(){
+        animationPlaying = true;
+        print("lose life");
         currentLives--;
-        gameObject.transform.GetChild(currentLives).gameObject.SetActive(false);
+        Animator animator = gameObject.transform.GetChild(currentLives).GetComponent<Animator>();
+        animator.SetBool("takeDamage", true);
+        Destroy (gameObject.transform.GetChild(currentLives).gameObject, 0.58f); 
+
+
     }
 }
