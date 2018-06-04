3d799b2b7dbe76efb469b5f3de4140a614faf947
simonlhopkins <simonlhopkins@gmail.com>
2018-06-03 20:51:16 -0400

    started heart Animation

---
+++ Assets/Scripts/healthManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthManager : MonoBehaviour {
+
+
+    HarmfulCollision harmfulCollision;
+    public GameObject heartGameObject;
+    int currentLives;
+
+	// Use this for initialization
+    void Start () {
+        
+        harmfulCollision = GetComponent<HarmfulCollision>();
+        currentLives = harmfulCollision.health;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if(harmfulCollision.Damage){
+        }
+	}
+}
